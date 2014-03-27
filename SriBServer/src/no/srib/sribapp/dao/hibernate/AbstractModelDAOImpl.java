bb5a312a622af42069cb5d246d4013cec1bacd9b
Sveinung <Sveinung@fede425a-ac20-4494-a16f-d7f81ad4c10f>
2014-03-27 14:14:38 +0000

    DAO interfaces

---
+++ SriBServer/src/no/srib/sribapp/dao/hibernate/AbstractModelDAOImpl.java
@@ -0,0 +1,9 @@
+package no.srib.sribapp.dao.hibernate;
+
+import no.srib.sribapp.dao.interfaces.AbstractModelDAO;
+import no.srib.sribapp.model.AbstractModel;
+
+
+class AbstractModelDAOImpl<T extends AbstractModel> implements AbstractModelDAO<T> {
+
+}
---
+++ SriBServer/src/no/srib/sribapp/dao/hibernate/PodcastDAOImpl.java
@@ -0,0 +1,8 @@
+package no.srib.sribapp.dao.hibernate;
+
+import no.srib.sribapp.dao.interfaces.PodcastDAO;
+import no.srib.sribapp.model.Podcast;
+
+public class PodcastDAOImpl extends AbstractModelDAOImpl<Podcast> implements PodcastDAO {
+
+}
