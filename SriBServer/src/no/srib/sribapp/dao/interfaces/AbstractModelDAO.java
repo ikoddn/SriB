984737920a65ce9c611797e538947a2d3c6e1d86
Sveinung <Sveinung@fede425a-ac20-4494-a16f-d7f81ad4c10f>
2014-03-27 14:40:01 +0000

    Javadoc on DAO interfaces

--- SriBServer/src/no/srib/sribapp/dao/interfaces/AbstractModelDAO.java
+++ SriBServer/src/no/srib/sribapp/dao/interfaces/AbstractModelDAO.java
@@ -5,6 +5,13 @@ import java.util.List;
 import no.srib.sribapp.dao.exception.DAOException;
 import no.srib.sribapp.model.AbstractModel;
 
+/**
+ * A Data Access Object for generic types. It should not be used directly, you
+ * should rather make a specialization based upon this.
+ * 
+ * @param <T>
+ *            - the type of the data.
+ */
 public interface AbstractModelDAO<T extends AbstractModel> {
 
     /**
--- SriBServer/src/no/srib/sribapp/dao/interfaces/PodcastDAO.java
+++ SriBServer/src/no/srib/sribapp/dao/interfaces/PodcastDAO.java
@@ -2,6 +2,12 @@ package no.srib.sribapp.dao.interfaces;
 
 import no.srib.sribapp.model.Podcast;
 
+/**
+ * A Data Access Object for Podcast entities.
+ * 
+ * @author Sveinung
+ *
+ */
 public interface PodcastDAO extends AbstractModelDAO<Podcast> {
 
 }
