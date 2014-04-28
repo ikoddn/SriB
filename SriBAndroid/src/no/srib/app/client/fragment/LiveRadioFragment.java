57d43eec4851007385ab7b7490730595c1ce131a
Sveinung <Sveinung@fede425a-ac20-4494-a16f-d7f81ad4c10f>
2014-04-28 10:32:37 +0000

    Play/pause indication

--- SriBAndroid/src/no/srib/app/client/fragment/LiveRadioFragment.java
+++ SriBAndroid/src/no/srib/app/client/fragment/LiveRadioFragment.java
@@ -5,11 +5,19 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LiveRadioFragment extends Fragment {
 
+	private boolean playing;
+	
+	public LiveRadioFragment() {
+		playing = false;
+	}
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -18,6 +26,25 @@ public class LiveRadioFragment extends Fragment {
 		TextView textView = (TextView) rootView
 				.findViewById(R.id.label_liveradio);
 		textView.setText("Live radio fragment");
+		
+		Button playButton = (Button) rootView.findViewById(R.id.button_liveradio);
+		playButton.setOnClickListener(new PlayButtonListener());
 		return rootView;
+	}
+	
+	private class PlayButtonListener implements OnClickListener {
+		
+		@Override
+		public void onClick(View button) {
+			playing = !playing;
+			
+			TextView label = (TextView) getActivity().findViewById(R.id.label_liveradio);
+			
+			if (playing) {
+				label.setText("Playing");
+			} else {
+				label.setText("Paused");
+			}
+		}
 	}
 }
