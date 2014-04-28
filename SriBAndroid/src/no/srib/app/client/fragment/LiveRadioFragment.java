7b9542f9da7d765dfef5f0664f41650c4dbf7c6a
Sveinung <Sveinung@fede425a-ac20-4494-a16f-d7f81ad4c10f>
2014-04-28 11:57:07 +0000

    Live radio playback/stop and pause (hardcoded URL)

--- SriBAndroid/src/no/srib/app/client/fragment/LiveRadioFragment.java
+++ SriBAndroid/src/no/srib/app/client/fragment/LiveRadioFragment.java
@@ -1,6 +1,11 @@
 package no.srib.app.client.fragment;
 
+import java.io.IOException;
+
 import no.srib.R;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnPreparedListener;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,12 +17,15 @@ import android.widget.TextView;
 
 public class LiveRadioFragment extends Fragment {
 
-	private boolean playing;
-	
+	private boolean paused;
+	private MediaPlayer mediaPlayer;
+
 	public LiveRadioFragment() {
-		playing = false;
+		paused = false;
+		mediaPlayer = new MediaPlayer();
+		mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
@@ -26,25 +34,77 @@ public class LiveRadioFragment extends Fragment {
 		TextView textView = (TextView) rootView
 				.findViewById(R.id.label_liveradio);
 		textView.setText("Live radio fragment");
-		
-		Button playButton = (Button) rootView.findViewById(R.id.button_liveradio);
+
+		Button playButton = (Button) rootView
+				.findViewById(R.id.button_liveradio_play);
 		playButton.setOnClickListener(new PlayButtonListener());
+
+		Button pauseButton = (Button) rootView
+				.findViewById(R.id.button_liveradio_pause);
+		pauseButton.setOnClickListener(new PauseButtonListener());
+
+		try {
+			mediaPlayer.setDataSource("http://radio.srib.no:8888/srib.mp3");
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		mediaPlayer.setOnPreparedListener(new MediaPlayerPreparedListener());
+
 		return rootView;
 	}
-	
+
 	private class PlayButtonListener implements OnClickListener {
-		
+
 		@Override
 		public void onClick(View button) {
-			playing = !playing;
-			
-			TextView label = (TextView) getActivity().findViewById(R.id.label_liveradio);
-			
-			if (playing) {
-				label.setText("Playing");
+			TextView label = (TextView) getActivity().findViewById(
+					R.id.label_liveradio);
+
+			if (paused) {
+				mediaPlayer.start();
+				paused = false;
+				label.setText("start");
+			} else if (mediaPlayer.isPlaying()) {
+				mediaPlayer.stop();
+				label.setText("stop");
 			} else {
-				label.setText("Paused");
+				mediaPlayer.prepareAsync();
+				label.setText("prepareAsync");
 			}
+		}
+	}
+
+	private class PauseButtonListener implements OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			TextView label = (TextView) getActivity().findViewById(
+					R.id.label_liveradio);
+
+			if (!paused && mediaPlayer.isPlaying()) {
+				mediaPlayer.pause();
+				label.setText("pause");
+				paused = true;
+			}
+		}
+	}
+
+	private class MediaPlayerPreparedListener implements OnPreparedListener {
+
+		@Override
+		public void onPrepared(MediaPlayer mp) {
+			mediaPlayer.start();
 		}
 	}
 }
