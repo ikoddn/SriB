d24334aa736f028d29f7765551c3a1a292e94b06
Sveinung <Sveinung@fede425a-ac20-4494-a16f-d7f81ad4c10f>
2014-05-01 10:09:02 +0000

    Android: HttpAsyncTask response handling implemented by a interface

--- SriBAndroid/src/no/srib/app/client/asynctask/HttpAsyncTask.java
+++ SriBAndroid/src/no/srib/app/client/asynctask/HttpAsyncTask.java
@@ -3,8 +3,6 @@ package no.srib.app.client.asynctask;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
-import no.srib.app.client.model.StreamSchedule;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
@@ -12,19 +10,17 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import android.os.AsyncTask;
 import android.util.Log;
 
 public class HttpAsyncTask extends AsyncTask<String, Void, String> {
 
 	private HttpClient httpClient;
+	private HttpResponseListener responseListener;
 
-	public HttpAsyncTask() {
+	public HttpAsyncTask(HttpResponseListener responseListener) {
 		httpClient = new DefaultHttpClient();
+		this.responseListener = responseListener;
 	}
 
 	@Override
@@ -59,29 +55,10 @@ public class HttpAsyncTask extends AsyncTask<String, Void, String> {
 
 	@Override
 	protected void onPostExecute(String result) {
-		if (result != null) {
-			Log.d("SriB", result);
-			ObjectMapper mapper = new ObjectMapper();
-			try {
-				StreamSchedule streamSchedule = mapper.readValue(result, StreamSchedule.class);
-				Log.d("SriB", "Name: " + streamSchedule.getName());
-				Log.d("SriB", "URL: " + streamSchedule.getUrl());
-				Log.d("SriB", "Time: " + streamSchedule.getTime().getTimeInMillis());
-			} catch (JsonParseException e) {
-				// TODO Auto-generated catch block
-				Log.e("SriB", e.getMessage());
-				e.printStackTrace();
-			} catch (JsonMappingException e) {
-				// TODO Auto-generated catch block
-				Log.e("SriB", e.getMessage());
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				Log.e("SriB", e.getMessage());
-				e.printStackTrace();
-			}
-		} else {
-			Log.d("SriB", "result == null");
-		}
+		responseListener.onResponse(result);
+	}
+
+	public interface HttpResponseListener {
+		void onResponse(String response);
 	}
 }
