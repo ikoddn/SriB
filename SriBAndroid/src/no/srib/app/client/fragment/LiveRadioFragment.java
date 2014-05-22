193706963cfb40d4def417d48988715adf783d15
Sveinung <Sveinung@fede425a-ac20-4494-a16f-d7f81ad4c10f>
2014-05-22 13:30:07 +0000

    Android: Using the TreeViewObserver's OnGlobalLayoutListener to add the background image

--- SriBAndroid/src/no/srib/app/client/fragment/LiveRadioFragment.java
+++ SriBAndroid/src/no/srib/app/client/fragment/LiveRadioFragment.java
@@ -10,10 +10,13 @@ import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -32,6 +35,7 @@ public class LiveRadioFragment extends Fragment {
 	private TextView programNameTextView;
 	private ImageButton playButton;
 	private DTImageView background;
+	private View rootView;
 
 	public static LiveRadioFragment newInstance(
 			OnClickListener infoClickListener) {
@@ -77,90 +81,74 @@ public class LiveRadioFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		final View root = inflater.inflate(R.layout.fragment_liveradio,
-				container, false);
+		rootView = inflater.inflate(R.layout.fragment_liveradio, container,
+				false);
 
-		root.post(new Runnable() {
-			public void run() {
-				int height = root.getHeight();
-				int width = root.getWidth();
-
-				background = (DTImageView) root
-						.findViewById(R.id.dtImageView_liveradio_background);
-
-				Activity activity = getParentFragment().getActivity();
-				Bitmap bitmap = BitmapUtil.decodeSampledBitmapFromResource(
-						activity.getResources(), R.drawable.layout, width,
-						height);
-
-				background.setBitmap(Bitmap.createScaledBitmap(bitmap, width,
-						height, true));
-			}
-		});
-
-		statusTextView = (TextView) root
+		statusTextView = (TextView) rootView
 				.findViewById(R.id.textview_liveradio_status);
-		streamTextView = (TextView) root
+		streamTextView = (TextView) rootView
 				.findViewById(R.id.textview_liveradio_stream);
-		programNameTextView = (TextView) root
+		programNameTextView = (TextView) rootView
 				.findViewById(R.id.textview_liveradio_programname);
 
-		Typeface appFont = Typeface.createFromAsset(root.getContext()
+		Typeface font = Typeface.createFromAsset(rootView.getContext()
 				.getAssets(), "fonts/clairehandbold.ttf");
-		statusTextView.setTypeface(appFont);
-		streamTextView.setTypeface(appFont);
-		programNameTextView.setTypeface(appFont);
+		statusTextView.setTypeface(font);
+		streamTextView.setTypeface(font);
+		programNameTextView.setTypeface(font);
 
-		playButton = (ImageButton) root
+		playButton = (ImageButton) rootView
 				.findViewById(R.id.button_liveradio_play);
-		ImageButton stopButton = (ImageButton) root
+		ImageButton stopButton = (ImageButton) rootView
 				.findViewById(R.id.button_liveradio_stop);
-		ImageButton infoButton = (ImageButton) root
+		ImageButton infoButton = (ImageButton) rootView
 				.findViewById(R.id.button_liveradio_info);
-		ImageButton twitterButton = (ImageButton) root
+		ImageButton twitterButton = (ImageButton) rootView
 				.findViewById(R.id.button_liveradio_twitter);
-		ImageButton instagramButton = (ImageButton) root
+		ImageButton instagramButton = (ImageButton) rootView
 				.findViewById(R.id.button_liveradio_instagram);
 
 		playButton.setOnClickListener(new PlayPauseButtonListener());
 		stopButton.setOnClickListener(new StopButtonListener());
 		twitterButton.setOnClickListener(new TwitterButtonListener());
 		instagramButton.setOnClickListener(new InstagramButtonListener());
+		infoButton.setOnClickListener(infoClickListener);
 
-		if (infoClickListener != null) {
-			infoButton.setOnClickListener(infoClickListener);
+		ViewTreeObserver observer = rootView.getViewTreeObserver();
+		if (observer.isAlive()) {
+			observer.addOnGlobalLayoutListener(new LayoutReadyListener());
 		}
 
 		float smallButtonWeight = 67.0f;
 
 		// Main vertical LinearLayout
-		ViewUtil.setWeight(R.id.view_liveradio_vspace1, root, 27.0f);
-		ViewUtil.setWeight(R.id.linearlayout_liveradio_info, root,
+		ViewUtil.setWeight(R.id.view_liveradio_vspace1, rootView, 27.0f);
+		ViewUtil.setWeight(R.id.linearlayout_liveradio_info, rootView,
 				smallButtonWeight);
-		ViewUtil.setWeight(R.id.view_liveradio_vspace2, root, 234.0f);
+		ViewUtil.setWeight(R.id.view_liveradio_vspace2, rootView, 234.0f);
 		ViewUtil.setWeight(programNameTextView, 40.0f);
-		ViewUtil.setWeight(R.id.view_liveradio_vspace3, root, 226.0f);
+		ViewUtil.setWeight(R.id.view_liveradio_vspace3, rootView, 226.0f);
 		ViewUtil.setWeight(playButton, 203.0f);
-		ViewUtil.setWeight(R.id.view_liveradio_vspace4, root, 68.0f);
+		ViewUtil.setWeight(R.id.view_liveradio_vspace4, rootView, 68.0f);
 		ViewUtil.setWeight(stopButton, smallButtonWeight);
-		ViewUtil.setWeight(R.id.view_liveradio_vspace5, root, 251.0f);
-		ViewUtil.setWeight(R.id.linearlayout_liveradio_social, root,
+		ViewUtil.setWeight(R.id.view_liveradio_vspace5, rootView, 251.0f);
+		ViewUtil.setWeight(R.id.linearlayout_liveradio_social, rootView,
 				smallButtonWeight);
-		ViewUtil.setWeight(R.id.view_liveradio_vspace6, root, 50.0f);
+		ViewUtil.setWeight(R.id.view_liveradio_vspace6, rootView, 50.0f);
 
 		// Horizontal LinearLayout for info button
-		ViewUtil.setWeight(R.id.view_liveradio_info_hspace1, root, 678.0f);
+		ViewUtil.setWeight(R.id.view_liveradio_info_hspace1, rootView, 678.0f);
 		ViewUtil.setWeight(infoButton, smallButtonWeight);
-		ViewUtil.setWeight(R.id.view_liveradio_info_hspace2, root, 35.0f);
+		ViewUtil.setWeight(R.id.view_liveradio_info_hspace2, rootView, 35.0f);
 
 		// Horizontal LinearLayout for social media buttons
-		ViewUtil.setWeight(R.id.view_liveradio_social_hspace1, root, 289.0f);
+		ViewUtil.setWeight(R.id.view_liveradio_social_hspace1, rootView, 289.0f);
 		ViewUtil.setWeight(instagramButton, smallButtonWeight);
-		ViewUtil.setWeight(R.id.view_liveradio_social_hspace2, root, 68.0f);
+		ViewUtil.setWeight(R.id.view_liveradio_social_hspace2, rootView, 68.0f);
 		ViewUtil.setWeight(twitterButton, smallButtonWeight);
-		ViewUtil.setWeight(R.id.view_liveradio_social_hspace3, root, 289.0f);
+		ViewUtil.setWeight(R.id.view_liveradio_social_hspace3, rootView, 289.0f);
 
-		return root;
+		return rootView;
 	}
 
 	@Override
@@ -185,6 +173,9 @@ public class LiveRadioFragment extends Fragment {
 		if (playing) {
 			setPauseIcon();
 		}
+
+		Log.d("SriB", "onActivityCreated");
+
 	}
 
 	@Override
@@ -200,6 +191,8 @@ public class LiveRadioFragment extends Fragment {
 		editor.putBoolean(KEY_IS_PLAYING, playing);
 
 		editor.commit();
+
+		Log.d("SriB", "onStop");
 	}
 
 	@Override
@@ -209,6 +202,20 @@ public class LiveRadioFragment extends Fragment {
 		if (background != null) {
 			background.cleanup();
 		}
+
+		Log.d("SriB", "onDestroyView");
+	}
+
+	public void setPauseIcon() {
+		playButton.setImageDrawable(getResources()
+				.getDrawable(R.drawable.pause));
+		playing = true;
+	}
+
+	public void setPlayIcon() {
+		playButton
+				.setImageDrawable(getResources().getDrawable(R.drawable.play));
+		playing = false;
 	}
 
 	public interface OnLiveRadioClickListener {
@@ -249,20 +256,29 @@ public class LiveRadioFragment extends Fragment {
 	private class TwitterButtonListener implements OnClickListener {
 
 		@Override
-		public void onClick(View arg0) {
+		public void onClick(View view) {
 			liveRadioClickListener.onTwitterClicked();
 		}
 	}
 
-	public void setPauseIcon() {
-		playButton.setImageDrawable(getResources()
-				.getDrawable(R.drawable.pause));
-		playing = true;
-	}
+	private class LayoutReadyListener implements OnGlobalLayoutListener {
 
-	public void setPlayIcon() {
-		playButton
-				.setImageDrawable(getResources().getDrawable(R.drawable.play));
-		playing = false;
+		@Override
+		public void onGlobalLayout() {
+			ViewUtil.removeOnGlobalLayoutListener(rootView, this);
+
+			int height = rootView.getHeight();
+			int width = rootView.getWidth();
+
+			background = (DTImageView) rootView
+					.findViewById(R.id.dtImageView_liveradio_background);
+
+			Activity activity = getParentFragment().getActivity();
+			Bitmap bitmap = BitmapUtil.decodeSampledBitmapFromResource(
+					activity.getResources(), R.drawable.layout, width, height);
+
+			background.setBitmap(Bitmap.createScaledBitmap(bitmap, width,
+					height, true));
+		}
 	}
 }
--- SriBAndroid/src/no/srib/app/client/util/ViewUtil.java
+++ SriBAndroid/src/no/srib/app/client/util/ViewUtil.java
@@ -1,6 +1,9 @@
 package no.srib.app.client.util;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
 
 public class ViewUtil {
@@ -14,5 +17,16 @@ public class ViewUtil {
 		LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) view
 				.getLayoutParams();
 		layoutParams.weight = weight;
+	}
+
+	@SuppressLint("NewApi")
+	@SuppressWarnings("deprecation")
+	public static void removeOnGlobalLayoutListener(View v,
+			ViewTreeObserver.OnGlobalLayoutListener listener) {
+		if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+			v.getViewTreeObserver().removeGlobalOnLayoutListener(listener);
+		} else {
+			v.getViewTreeObserver().removeOnGlobalLayoutListener(listener);
+		}
 	}
 }
