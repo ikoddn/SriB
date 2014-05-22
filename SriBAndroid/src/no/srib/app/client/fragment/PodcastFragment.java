9c227c22139022a1c8818b6cd011bcbb294aaf4b
Sveinung <Sveinung@fede425a-ac20-4494-a16f-d7f81ad4c10f>
2014-05-22 17:22:45 +0000

    Android: Removed a TextView element from PodcastFragment

--- SriBAndroid/src/no/srib/app/client/fragment/PodcastFragment.java
+++ SriBAndroid/src/no/srib/app/client/fragment/PodcastFragment.java
@@ -2,7 +2,6 @@ package no.srib.app.client.fragment;
 
 import no.srib.app.client.R;
 import android.app.Activity;
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -12,51 +11,48 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.GridView;
 import android.widget.Spinner;
-import android.widget.TextView;
-
 
 public class PodcastFragment extends Fragment {
 
-
 	private Spinner spinner = null;
 	private GridView podcastGridView = null;
-	private OnPodcastFragmentReadyListener readyListener = null; 
+	private OnPodcastFragmentReadyListener readyListener = null;
 	private OnItemClickListener podcastClickedListener = null;
 	private OnItemSelectedListener spinnerSelectedListener = null;
 
+	public PodcastFragment() {
 
-	public PodcastFragment(){
-		
-		
 	}
-	
-	public GridView getGridView(){
-		
+
+	public GridView getGridView() {
+
 		return podcastGridView;
 	}
-	
+
 	public void setPodCastClickedListener(
 			OnItemClickListener podcastClickListener) {
 		this.podcastClickedListener = podcastClickListener;
 	}
-	
+
 	public void setSpinnerListSelectedListener(
 			OnItemSelectedListener spinnerListClickListener) {
 		this.spinnerSelectedListener = spinnerListClickListener;
 	}
-	
-	public void setSpinnerListAdapter(no.srib.app.client.adapter.SpinnerAdapter spinnerListAdapter) {
+
+	public void setSpinnerListAdapter(
+			no.srib.app.client.adapter.SpinnerAdapter spinnerListAdapter) {
 		if (spinner != null) {
 			spinner.setAdapter(spinnerListAdapter);
 		}
 	}
-	
-	public void setGridArrayAdapter(no.srib.app.client.adapter.GridArrayAdapter gridViewAdapter2) {
+
+	public void setGridArrayAdapter(
+			no.srib.app.client.adapter.GridArrayAdapter gridViewAdapter2) {
 		if (podcastGridView != null) {
 			podcastGridView.setAdapter(gridViewAdapter2);
 		}
 	}
-	
+
 	@Override
 	public void onAttach(Activity activity) {
 		super.onAttach(activity);
@@ -66,41 +62,31 @@ public class PodcastFragment extends Fragment {
 			readyListener = null;
 		}
 	}
-	
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_podcast, container,
 				false);
 		setRetainInstance(true);
-		Typeface appFont = Typeface.createFromAsset(rootView.getContext().getAssets(), "fonts/clairehandbold.ttf");
-		TextView textView1 = (TextView) rootView
-				.findViewById(R.id.label_program_name);
-		textView1.setTypeface(appFont);
-		textView1.setText("Program navn:");
-	
+
 		spinner = (Spinner) rootView.findViewById(R.id.spinner1);
-		podcastGridView = (GridView) rootView.findViewById(R.id.gridView_podcastList);
-		
-		
+		podcastGridView = (GridView) rootView
+				.findViewById(R.id.gridView_podcastList);
+
 		if (readyListener != null) {
 			readyListener.onPodcastFragmentReady();
 		}
-		
+
 		spinner.setOnItemSelectedListener(spinnerSelectedListener);
 		podcastGridView.setOnItemClickListener(podcastClickedListener);
-		
-		
+
 		return rootView;
-		
+
 	}
-	
+
 	public interface OnPodcastFragmentReadyListener {
 		void onPodcastFragmentReady();
 	}
-	
-	
+
 }
-
-
