422fc814623d5397d36e7b1cbea3cac7aad33bfd
ikoddn <ikonage@gmail.com>
2014-07-29 03:07:57 +0200

    Android: ButterKnife "view injection" in InfoFragment.

--- SriBAndroid/src/no/srib/app/client/fragment/InfoFragment.java
+++ SriBAndroid/src/no/srib/app/client/fragment/InfoFragment.java
@@ -12,15 +12,23 @@ import android.os.Bundle;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 public class InfoFragment extends BaseFragment {
 
+	@InjectView(R.id.dtimageview_info) DTImageView image;
+	@InjectView(R.id.button_info_facebook) ImageButton facebookButton;
+	@InjectView(R.id.button_info_spotify) ImageButton spotifyButton;
+	@InjectView(R.id.button_info_sribwebsite) ImageButton sribWebsiteButton;
+	@InjectView(R.id.textview_info1) TextView infoText1;
+	@InjectView(R.id.textview_info2) TextView infoText2;
+
 	private Bitmap imageBitmap;
-	private DTImageView image;
 	private OnInfoClickListener infoClickListener;
 
 	public InfoFragment() {
@@ -44,27 +52,10 @@ public class InfoFragment extends BaseFragment {
 
 		View rootView = inflater.inflate(R.layout.fragment_info, container,
 				false);
+		ButterKnife.inject(this, rootView);
 
-		ImageButton facebookButton = (ImageButton) rootView
-				.findViewById(R.id.button_info_facebook);
-		ImageButton spotifyButton = (ImageButton) rootView
-				.findViewById(R.id.button_info_spotify);
-		ImageButton sribwwwButton = (ImageButton) rootView
-				.findViewById(R.id.button_info_sribwww);
-
-		facebookButton.setOnClickListener(new FacebookButtonListener());
-		spotifyButton.setOnClickListener(new SpotifyButtonListener());
-		sribwwwButton.setOnClickListener(new SribwwwButtonListener());
-
-		TextView textView1 = (TextView) rootView
-				.findViewById(R.id.textview_info1);
-
-		textView1.setText(Html.fromHtml(getString(R.string.textView_info_1)));
-
-		TextView textView2 = (TextView) rootView
-				.findViewById(R.id.textview_info2);
-
-		textView2.setText(Html.fromHtml(getString(R.string.textView_info_2)));
+		infoText1.setText(Html.fromHtml(getString(R.string.textView_info_1)));
+		infoText2.setText(Html.fromHtml(getString(R.string.textView_info_2)));
 
 		if (imageBitmap == null) {
 			final int imageSizeDPI = 200;
@@ -75,7 +66,6 @@ public class InfoFragment extends BaseFragment {
 					imageSizePixels, true);
 		}
 
-		image = (DTImageView) rootView.findViewById(R.id.dtImageView_info);
 		image.setBitmap(imageBitmap);
 
 		return rootView;
@@ -91,33 +81,24 @@ public class InfoFragment extends BaseFragment {
 		}
 	}
 
-	private class FacebookButtonListener implements OnClickListener {
-
-		@Override
-		public void onClick(View button) {
-			if (infoClickListener != null) {
-				infoClickListener.onFacebookClicked();
-			}
-		}
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		ButterKnife.reset(this);
 	}
 
-	private class SpotifyButtonListener implements OnClickListener {
-
-		@Override
-		public void onClick(View button) {
-			if (infoClickListener != null) {
-				infoClickListener.onSpotifyClicked();
-			}
-		}
+	@OnClick(R.id.button_info_facebook)
+	void facebookButtonClicked() {
+		infoClickListener.onFacebookClicked();
 	}
 
-	private class SribwwwButtonListener implements OnClickListener {
+	@OnClick(R.id.button_info_spotify)
+	void spotifyButtonClicked() {
+		infoClickListener.onSpotifyClicked();
+	}
 
-		@Override
-		public void onClick(View button) {
-			if (infoClickListener != null) {
-				infoClickListener.onSribWebsiteClicked();
-			}
-		}
+	@OnClick(R.id.button_info_sribwebsite)
+	void sribWebsiteButtonClicked() {
+		infoClickListener.onSribWebsiteClicked();
 	}
 }
